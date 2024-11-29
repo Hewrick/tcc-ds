8b6406587c92edc4d71c9bd8529ac0b964448b4e
Pedro Ribeiro <pedro0709.0709@outlook.com>
2024-11-28 22:07:08 -0300

    da uma olhada ai, Marcos

--- Software/app.js
+++ Software/app.js
@@ -2,13 +2,14 @@
  * -> Comando para installar o Three.js (usar no console do computador): npm install three
 */
 
+const simulacao = document.getElementById("simulacao");
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, simulacao.offsetWidth / simulacao.offsetHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-//tentar: render.setSize(window.document.body.simulacao);
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+//tentar: render.setSize(window.document.body.simulacao.innerWidth, window.document.body.simulacao.innerHeight);
+renderer.setSize(simulacao.offsetWidth, simulacao.offsetHeight);
+simulacao.appendChild(renderer.domElement);
 
 camera.position.z = 15;
 
